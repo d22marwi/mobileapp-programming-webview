dcb231371f73003012500f8069ac5b5210a3a59b
d22marwi <d22marwi@student.his.se>
2024-04-12 10:57:21 +0200

    färdig!

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -17,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private WebSettings webSettings;
 
     public void showExternalWebPage(){
-
+        myWebView.loadUrl("https://google.se");
         }
 
     public void showInternalWebPage(){
-        // TODO: Add your code for showing internal web page here
+        myWebView.loadUrl("file:///android_asset/maria.html");
     }
 
     @Override
@@ -79,11 +79,13 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
+            showExternalWebPage();
             Log.d("==>","Will display external web page");
             return true;
         }
 
         if (id == R.id.action_internal_web) {
+            showInternalWebPage();
             Log.d("==>","Will display internal web page");
             return true;
         }
